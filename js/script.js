8febe829c97aab15e15e48d8119a569af28046f8
SimoneDUrso <simone.durso98@gmail.com>
2024-07-16 14:25:01 +0200

    [UPD] Added image to JS recalled in HTML file with v-bind. Bonus completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: `Benvenuto nella pagina!`
+            message: `Benvenuto nella pagina!`,
+            image: `https://static1.srcdn.com/wordpress/wp-content/uploads/2024/02/elden-ring-shadow-of-the-erdtree-dlc-messmer-the-impaler.jpg`
         }
     }
 }).mount("#app")
